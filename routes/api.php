a26a84ac2ab397ea43c1756712df6434bc2f6c67
Andor <andorhorvat@gmail.com>
2020-05-28 15:57:13 +0200

    JWT installed

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+});
