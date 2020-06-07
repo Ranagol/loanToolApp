a9c94753422b813b8a5be1c5b6d42bad1405ad17
Andor <andorhorvat@gmail.com>
2020-06-07 14:29:27 +0200

    Customer seeder created

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Customer;
+use Faker\Generator as Faker;
+
+$factory->define(Customer::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'city' => $faker->city,
+        'address' => $faker->streetAddress,
+        'phone' => $faker->e164PhoneNumber,
+        'scan_doc' => 'https://blog.goranrakic.com/uploads/jfreesteelgui.png',
+        'blacklist' => false,
+        'comments' => $faker->realText(180),
+    ];
+});
