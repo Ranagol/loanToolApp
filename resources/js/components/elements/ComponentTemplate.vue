f2d83037653c26316be1907af459beb569497041
Andor <andorhorvat@gmail.com>
2020-06-18 19:26:08 +0200

    ComponentTemplate created

---
+++ resources/js/components/elements/ComponentTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>ComponentTemplate</h3>
+
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+    name: 'ComponentTemplate',
+    computed: {
+        ...mapGetters(['customers', 'tools', 'invoices', 'invoiceitems']),
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        ...mapActions(['getCustomers', 'getTools', 'getInvoices', 'getInvoiceitems']),
+    },
+    
+    created(){
+        this.getCustomers();
+        this.getTools();
+        this.getInvoices();
+        this.getInvoiceitems();
+    }
+}
+</script>
